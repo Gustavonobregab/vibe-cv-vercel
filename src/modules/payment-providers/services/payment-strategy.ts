11068ec500a05fdf514e2255aab3de5a6489aae5
Gustavo Da Nóbrega Brandão <gustavonobg@gmail.com>
2025-05-13 19:00:28 -0300

    fix: removendo classe do strategy

--- src/modules/payment-providers/services/payment-strategy.ts
+++ src/modules/payment-providers/services/payment-strategy.ts
@@ -1,10 +1,11 @@
+// payment-strategy.factory.ts
 import { PaymentStrategy } from '../types/payment-strategy.types';
 import { CreatePaymentDto } from '../../payments/types/payment.types';
 import AbacatePaymentService from '../abacate/abacate.service';
 import PagarmePaymentService from '../pagarme/pagarme.service';
 
-export class PaymentStrategyFactory {
-  static getStrategy(payment: CreatePaymentDto): PaymentStrategy {
+const PaymentStrategyFactory = {
+  getStrategy(payment: CreatePaymentDto): PaymentStrategy {
     switch (payment.paymentMethod) {
       case 'abacate':
         return new AbacatePaymentService();
@@ -14,4 +15,6 @@ export class PaymentStrategyFactory {
         throw new Error(`Unsupported payment provider: ${payment.paymentMethod}`);
     }
   }
-}
+};
+
+export default PaymentStrategyFactory;
